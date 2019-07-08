7a7dc657f12f865aa3e8b0d6e7f37f24994c1c95
WangPengchao <wangpengchao__just_thinking@outlook.com>
2019-07-08 16:40:38 +0800

    2019年7月8日16:39:32 学习flask框架

--- app.py
+++ app.py
@@ -1,28 +1,51 @@
 from flask import Flask
-from flask import url_for
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask import make_response
+from flask import redirect
+from flask import abort
+from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    name = StringField()
+
+
+@app.errorhandler(404)
+def page_not_fount(e):
+    return render_template('404.html'), 404
+
+
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello'
+def index():
+    # response = make_response('<h1>hello world!!!!!</h1>')
+    # response.set_cookie('answer', '42')
+    # return response
+    return render_template('index.html')
+
 
 @app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % name
+def user(name):
+    # return '<h1>hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello_world'))
-    print(url_for('user_page', name='wpc'))
-    print(url_for('user_page', name='ll'))
-    print(url_for('test_url_for'))
-    print(url_for('test_url_for', num=2))
-    return 'Test page'
 
 if __name__ == '__main__':
     app.run()
+
 
 
 
